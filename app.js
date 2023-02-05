c67177c5a5183747b858198d0af34f5969843f1d
Lap4Dev <104302237+Lap4Dev@users.noreply.github.com>
2023-02-05 22:19:19 +0200

    read-query-params

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
+tg.MainButton.show();
 
 let yandexMap = document.querySelector('iframe');
 
@@ -8,6 +9,13 @@ function resizeMap() {
     yandexMap.width = window.innerWidth + 'px';
 }
 
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+if(Object.entries(params).length) {
+    if('show_hotel' in params)
+        tg.sendData(params.show_hotel);
+        // alert();
+}
 
 window.onresize = resizeMap;
 resizeMap();
