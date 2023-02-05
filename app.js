b1fab6984c06c83ac92d8a8f9d49bd65ecc96156
Lap4Dev <104302237+Lap4Dev@users.noreply.github.com>
2023-02-05 22:43:52 +0200

    tg sendData

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
-tg.MainButton.show();
 
+let showHotel;
 let yandexMap = document.querySelector('iframe');
 
 function resizeMap() {
     yandexMap.height = window.innerHeight + 'px';
     yandexMap.width = window.innerWidth + 'px';
 }
+window.onresize = resizeMap;
 
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 if(Object.entries(params).length) {
     if('show_hotel' in params)
-        alert(params.show_hotel);
         Telegram.WebApp.close();
         tg.sendData(params.show_hotel);
-        // alert();
+        tg.MainButton.setText('Посмотреть подробную информацию').show();
 }
 
-window.onresize = resizeMap;
+Telegram.WebApp.onEvent('mainButtonClicked', () => {
+    if(showHotel) {
+        tg.sendData(showHotel);
+    }
+});
+
 resizeMap();
